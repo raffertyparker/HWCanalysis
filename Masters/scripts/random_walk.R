5d769bac6432b74ea32a045a148a3a60d1b0c27a
Rafferty Parker <raffertyjparker@gmail.com>
2019-09-13 12:18:33 +1200

    extract plottable data and create ggplot

--- Masters/scripts/random_walk.R
+++ Masters/scripts/random_walk.R
@@ -1,18 +1,31 @@
 if (!exists("dFile")){
   dFile <- "~/HWCanalysis/Masters/data/" 
 }
-if (!exists("mFile")){
- mFile <- "~/HWCanalysis/Masters/models/" 
-} 
+if (!exists("DT_hh")){
+load(paste0(dFile, "DT_hh.Rda"))
+}
 
-load(paste0(dFile, "DT_hh"))
+library(dplyr)
+library(data.table)
 
 for (house in unique(DT_hh$linkID)){
   p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
+  pMdl <- cbind(DT_hh$hHour[DT_hh$linkID == house], p$x, p$fitted)
+  pMdl <- data.table(pMdl)
+  names(pMdl) <- c("Time", "Actual", "Fitted")
+  pMdl$Time <- lubridate::as_datetime(pMdl$Time, tz = 'Pacific/Auckland')
   assign(house, p)
   get(house) %>%
     save(file = paste0(dFile, "models/randomWalk/", house, "_at_30.Rda"))
+  assign(house, pMdl)
+  get(house) %>%
+    save(file = paste0(dFile, "models/randomWalk/", house, "_at_30_for_plotting.Rda"))
 }
+
+library(ggplot2)
+p <- ggplot(data = rf_06[1:48, ], aes(x = Time, y = Actual)) +
+  geom_line(col = 'red') +
+  geom_line(aes(y = Fitted), col = 'blue') 
 
 #p1a<-ggplot(data=,aes(x=date,y=observed)) 
 #p1a<-p1a+geom_line(col='red')
