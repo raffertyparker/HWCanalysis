d9303c0da4559a1259f0746da80cdb83ecea4411
Rafferty Parker <raffertyjparker@gmail.com>
2019-09-25 16:07:14 +1200

    fixed facet_grid plot added proc.time

--- Masters/scripts/random_walk.R
+++ Masters/scripts/random_walk.R
@@ -2,7 +2,7 @@ if (!exists("dFile")){
   dFile <- "~/HWCanalysis/Masters/data/" 
 }
 if (!exists("pFile")){
-  dFile <- "~/HWCanalysis/Masters/plots/" 
+  pFile <- "~/HWCanalysis/Masters/plots/" 
 }
 if (!exists("DT_hh")){
 load(paste0(dFile, "DT_hh.Rda"))
@@ -12,6 +12,7 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 
+proc.time() <- stm
 for (house in unique(DT_hh$linkID)){
   p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
   pMdl <- cbind(DT_hh$hHour[DT_hh$linkID == house], p$x, p$fitted)
@@ -32,6 +33,13 @@ for (house in unique(DT_hh$linkID)){
   ggsave(filename = paste0(pFile, "randomWalk/", house, "RW.pdf"))
 }
 
+# This times the model
+proc.time() <- proc
+for (house in unique(DT_hh$linkID)){
+  p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
+}
+proc <- proc.time()
+save(proc, file=paste0(dFile, "models/randomWalk/procTime.Rda"))
 # The following was an attempt to make a facet_wrap of 4 households to show
 # variety of model fidelity. Unfortunately there is an unacceptable loss
 # of detail in the plot output.
@@ -43,21 +51,12 @@ rf_11$linkID <- "rf_11"
 
 RWplotDT <- rbind(rf_40, rf_13, rf_22, rf_11)
 RWplotDT <- dplyr::arrange(RWplotDT, Time)
+RWplotexample <- RWplotDT[48:(48*9), ]
 
-p <- ggplot(data = RWplotDT[48:(48*9), ], aes(x = Time, group = linkID)) +
+p <- ggplot(data = RWplotDT[48:(48*9), ], aes(x = Time)) +
   geom_line(aes(y = value, colour = variable)) +
   facet_grid(rows = vars(linkID), scales = "free_y")
   #facet_wrap(. ~ linkID, scales = "free")
 p + labs(y = "Power (W)", colour = "")
 ggsave(filename = paste0(pFile, "randomWalk/fourHouses.pdf"))
 
-#load(paste0(dFile, "models/randomWalk/", house, "_at_30_for_plotting.Rda"))
- 
-
-#p1a<-ggplot(data=,aes(x=date,y=observed)) 
-#p1a<-p1a+geom_line(col='red')
-#p1a<-p1a+geom_line(aes(y=fitted),col='blue')
-#p1a<-p1a+geom_line(aes(y=forecast))+geom_ribbon(aes(ymin=lo95,ymax=hi95),alpha=.25)
-#p1a<-p1a+scale_x_date(name='',breaks='1 year',minor_breaks='1 month',labels=date_format("%b-%y"),expand=c(0,0))
-#p1a<-p1a+scale_y_continuous(name='Units of Y')
-#p1a<-p1a+opts(axis.text.x=theme_text(size=10),title='Arima Fit to Simulated Data\n (black=forecast, blue=fitted, red=data, shadow=95% conf. interval)')
