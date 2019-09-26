23a39daa96620622dba4c29e76feb3d0fcdca0c2
Rafferty Parker <raffertyjparker@gmail.com>
2019-09-27 11:00:37 +1200

    added comments and changed name of output files

--- Masters/scripts/random_walk.R
+++ Masters/scripts/random_walk.R
@@ -12,34 +12,38 @@ library(dplyr)
 library(data.table)
 library(ggplot2)
 
-proc.time() <- stm
+sdResRW <- list()
 for (house in unique(DT_hh$linkID)){
-  p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
-  pMdl <- cbind(DT_hh$hHour[DT_hh$linkID == house], p$x, p$fitted)
+  p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1) # compute forecast
+  sdResRW[[house]] <- sd(resid(p), na.rm = TRUE)
+  pMdl <- cbind(DT_hh$hHour[DT_hh$linkID == house], p$x, p$fitted) # create new df of time, actual, fitted
   pMdl <- data.table(pMdl)
   names(pMdl) <- c("Time", "Actual", "Fitted")
   pMdl$Time <- lubridate::as_datetime(pMdl$Time, tz = 'Pacific/Auckland')
-  pMdl <- melt(pMdl, id = "Time")
-  pMdl <- dplyr::arrange(pMdl, Time)
-  assign(house, p)
-  get(house) %>%
+  pMdl <- melt(pMdl, id = "Time")  # reshapes to format more conducive to ggplot
+  pMdl <- dplyr::arrange(pMdl, Time) # rearranges chronologically
+  assign(paste0(house, "_model"), p) # assigns unique variable name
+  get(paste0(house, "_model")) %>%
     save(file = paste0(dFile, "models/randomWalk/", house, "_at_30.Rda"))
-  assign(house, pMdl)
-  get(house) %>%
-    save(file = paste0(dFile, "models/randomWalk/", house, "_at_30_for_plotting.Rda"))
-  p <- ggplot(data = get(house)[48:(48*3), ], aes(x = Time)) +
+  assign(paste0(house, "_for_plotting"), pMdl)
+  get(paste0(house, "_model")) %>%
+    save(file = paste0(dFile, "models/randomWalk/plotting/", house, "_at_30_for_plotting.Rda"))
+  p <- ggplot(data = get(paste0(house, "_for_plotting"))[48:(48*3), ], aes(x = Time)) +
     geom_line(aes(y = value, colour = variable)) +
     labs(y = "Power (W)", colour = "")
   ggsave(filename = paste0(pFile, "randomWalk/", house, "RW.pdf"))
 }
+sdResRW <- as.data.frame(sdResRW)
+sdResRW <- as.data.frame(t(sdResRW)) # transposes for ease of computation
+save(sdResRW, file = paste0(dFile, "models/randomWalk/sdResRW.Rda"))
 
 # This times the model
-proc.time() <- proc
+proc.time() <- RWproc
 for (house in unique(DT_hh$linkID)){
   p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
 }
-proc <- proc.time()
-save(proc, file=paste0(dFile, "models/randomWalk/procTime.Rda"))
+RWproc <- proc.time()
+save(RWproc, file=paste0(dFile, "models/randomWalk/procTime.Rda"))
 # The following was an attempt to make a facet_wrap of 4 households to show
 # variety of model fidelity. Unfortunately there is an unacceptable loss
 # of detail in the plot output.
@@ -51,12 +55,14 @@ rf_11$linkID <- "rf_11"
 
 RWplotDT <- rbind(rf_40, rf_13, rf_22, rf_11)
 RWplotDT <- dplyr::arrange(RWplotDT, Time)
-RWplotexample <- RWplotDT[48:(48*9), ]
+#RWplotexample <- RWplotDT[48:(48*9), ]
 
 p <- ggplot(data = RWplotDT[48:(48*9), ], aes(x = Time)) +
   geom_line(aes(y = value, colour = variable)) +
   facet_grid(rows = vars(linkID), scales = "free_y")
-  #facet_wrap(. ~ linkID, scales = "free")
+#facet_wrap(. ~ linkID, scales = "free")
 p + labs(y = "Power (W)", colour = "")
+
+ggplot_build(p)$data
 ggsave(filename = paste0(pFile, "randomWalk/fourHouses.pdf"))
 
