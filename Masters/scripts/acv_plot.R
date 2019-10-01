b0900fb50c84655751e5792a4e81b883307b858c
Rafferty Parker <raffertyjparker@gmail.com>
2019-10-02 09:38:48 +1300

    Minor changes to enhance reproducibility

--- Masters/scripts/acv_plot.R
+++ Masters/scripts/acv_plot.R
@@ -1,32 +1,31 @@
 #This created the acv plots
-
+if (!exists("dFile")){
+  dFile <- "~/HWCanalysis/Masters/data/" 
+}
+if (!exists("pFile")){
+  pFile <- "~/HWCanalysis/Masters/plots/" 
+}
 # Load the processed dataframe
 if (!exists("DT")){
-  load("~/HWCanalysis/Masters/data/DT.Rda")
+  load(paste0(dFile, "DT.Rda"))
 }
 
 for (house in unique(DT$linkID)){
   q <- forecast::Acf(DT$HWelec[DT$linkID == house], lag.max = 60*24*7, # one week
                      type = "correlation", 
                      plot = FALSE, na.action = na.pass)
+  names(q$acf) <- house
   assign(house, data.frame(q$acf))
-  if (house == "rf_01"){
-    acvDT <- data.frame(q$acf)
-  } else {
-  bind_cols(acvDT, eval(as.name(house)))
-  }
 }
 
-acvDT <- bind_cols(rf_01,rf_02,rf_06,rf_08,rf_11,rf_12,rf_13,rf_14,rf_15b,rf_16,rf_17a,
-          rf_18,rf_20,rf_22,rf_23,rf_24,rf_25,rf_27,rf_29,
-          rf_30,rf_31, rf_32,rf_33,rf_34,rf_35,
-          rf_36,rf_37,rf_38,rf_39,rf_40,rf_42,rf_44,rf_45)
+acvDT <- bind_cols(rf_01,rf_06,rf_08,rf_11,rf_13,rf_14,
+                   rf_15b,rf_17a,rf_22,rf_24,rf_25,rf_27,
+                   rf_29,rf_30,rf_31, rf_32,rf_33,rf_34,
+                   rf_35,rf_36,rf_37,rf_38,rf_39,rf_40,
+                   rf_42,rf_44,rf_45)
 
-names(acvDT) <- c("rf_01","rf_02","rf_06","rf_08","rf_11","rf_12","rf_13","rf_14",
-                  "rf_15b","rf_16","rf_17a","rf_18","rf_20","rf_22","rf_23","rf_24",
-                  "rf_25","rf_27","rf_29","rf_30","rf_31","rf_32","rf_33",
-                  "rf_34","rf_35","rf_36","rf_37","rf_38","rf_39","rf_40","rf_42",
-                  "rf_44","rf_45") 
+names(acvDT) <- unique(DT$linkID)
+  
 
 acvDT$lag <- seq(from = 1, to = 60*24*7 + 1)
 acvDT$lag <- as.character(acvDT$lag) # Necessary for correct melt
@@ -44,11 +43,11 @@ acvDT$lag <- as.character(acvDT$lag) # Necessary for correct melt
 acv_DT <- reshape2::melt(acvDT)
 acv_DT$lag <- as.numeric(acv_DT$lag)
 
-save(acv_DT, file = "~/HWCanalysis/Masters/data/acv_DT.Rda")
-#load("~/HWCanalysis/Masters/data/acv_DT.Rda")
+save(acv_DT, file = paste0(dFile, "acv_DT.Rda"))
+#load(paste0(dFile, "acv_DT.Rda"))
 
 ggplot(acv_DT, aes(lag,value,colour=variable)) +
        geom_line() + 
   labs(x = "Lag (minutes)", y = "Autocovariance")
-ggsave("~/HWCanalysis/Masters/plots/acfAllHouses.pdf")
+ggsave(paste0(pFile, "acfAllHouses.pdf"))
 ######################################
