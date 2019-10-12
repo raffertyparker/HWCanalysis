2848f51cee9d1fe8ab04be02774fb06ed40cea02
Rafferty Parker <raffertyjparker@gmail.com>
2019-10-12 14:30:57 +1300

    Many additions, functional but not yet plotting efficiently

--- Masters/scripts/AR.R
+++ Masters/scripts/AR.R
@@ -3,35 +3,55 @@
 if (!exists("dFile")){
   dFile <- "~/HWCanalysis/Masters/data/" 
 }
-
-library(forecast)
-library(xts)
-
-for (house in unique(DT_hh$linkID)){
-  load(paste0(dFile, "households/fitting/", house, "_at_30_min_for_fitting.Rda"))
-  assign(paste0("ts_", house, "_fitting"), as.xts(get(paste0(house, "_at_30_min_for_fitting"))))
-  assign(paste0("ts_", house, "_testing"), as.xts(get(paste0(house, "_at_30_min_for_validating"))))
-
-  # Create ARIMA model from training data
-  assign(paste0(house, "_30_min_ARIMA"), auto.arima(get(paste0("ts_", house, "_fitting"))$HWelec, 
-                                                  stepwise = FALSE, approximation = FALSE)) 
-  # Validate model from test data
-  assign(paste0(house, "_30_min_ARIMA_validate"), Arima(get(paste0("ts_", house, "_testing"))$HWelec, 
-                                                      model = get(paste0(house, "_30_min_ARIMA"))))
+if (!exists("pFile")){
+  pFile <- "~/HWCanalysis/Masters/plots/" 
+}
+if (!exists("sFile")){
+  sFile <- "~/HWCanalysis/Masters/scripts/" 
 }
 
-test <- fitted(rf_06_30_min_ARIMA_validate)
-test2 <- rf_06_30_min_ARIMA_validate$x
-names(test2) <- "actual"
-test2$fitted <- rf_06_30_min_ARIMA_validate$fitted
+source(paste0(sFile, "plot_model.R"))
+library(forecast)
+library(xts)
+library(data.table)
+library(ggplot2)
 
-################# UP TO HERE ####################
-ggplot2::ggplot()
+load(paste0(dFile, "houses.Rda"))
 
-plot(rf_06_30_min_ARIMA_validate$x,col="red")
-lines(fitted(rf_06_30_min_ARIMA_validate),col="blue")
+for (house in houses){
+  assign(paste0(house, "_at_30_min_for_fitting"), 
+         as.data.table(read_csv(paste0("Masters/data/households/fitting/", 
+                                       house,"_at_30_min_for_fitting.csv"))))
+  assign(paste0(house, "_at_30_min_for_validating"), 
+         as.data.table(read_csv(paste0("Masters/data/households/validating/", 
+                                       house,"_at_30_min_for_validating.csv"))))
+  assign(paste0("ts_", house, "_fitting"), 
+         as.xts(get(paste0(house, "_at_30_min_for_fitting"))))
+  assign(paste0("ts_", house, "_testing"), 
+         as.xts(get(paste0(house, "_at_30_min_for_validating"))))
 
-plot(fitted(rf_06_30_min_ARIMA_validate),col="blue")
+  # Create ARIMA model from training data
+  assign(paste0(house, "_30_min_ARIMA"), 
+         auto.arima(get(paste0("ts_", house, "_fitting"))$HWelec, 
+                    stepwise = FALSE, approximation = FALSE)) %>% 
+    save(file = paste0(dFile, "models/ARIMA/", house, "_fitted_model.Rda"))
+  # Validate model from test data
+  assign(paste0(house, "_30_min_ARIMA_validate"), 
+         Arima(get(paste0("ts_", house, "_testing"))$HWelec, 
+               model = get(paste0(house, "_30_min_ARIMA")))) %>%
+    save(file = paste0(dFile, "models/ARIMA/", house, "_validated_model.Rda"))
+}
 
-fit2 <- ets(testdata, model=fit)
-onestep <- fitted(fit2)
+# Make plot for demonstration purposes
+plotModel(get(paste0(house, "_30_min_ARIMA_validate")), "ARIMA")
+
+# NEED TO GET LIST AS FULL FILENAME
+# THEN LOAD FILES
+# THEN APPLY plotModel()
+
+#list1 <- list.files(paste0(dFile, "models/ARIMA/"), pattern = "_validated_model.Rda")
+#list2 <- append(paste0(dFile, "models/ARIMA/"), list1)
+#lapply(list, load)
+#lapply(list, plotModel("ARIMA"))
+
+
