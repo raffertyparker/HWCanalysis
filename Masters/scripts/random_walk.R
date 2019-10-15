43301cbe780d13fabc19f76c036932e44bfc7a84
Rafferty Parker <raffertyjparker@gmail.com>
2019-10-15 13:55:41 +1300

    updated to use plotModel function

--- Masters/scripts/random_walk.R
+++ Masters/scripts/random_walk.R
@@ -4,6 +4,9 @@ if (!exists("dFile")){
 if (!exists("pFile")){
   pFile <- "~/HWCanalysis/Masters/plots/" 
 }
+if (!exists("sFile")){
+  sFile <- "~/HWCanalysis/Masters/scripts/" 
+}
 if (!exists("DT_hh")){
 load(paste0(dFile, "DT_hh.Rda"))
 }
@@ -11,35 +14,26 @@ load(paste0(dFile, "DT_hh.Rda"))
 library(dplyr)
 library(data.table)
 library(ggplot2)
+library(xts)
+source(paste0(sFile, "plot_model.R"))
 
-sdResRW <- list()
-for (house in unique(DT_hh$linkID)){
+#sdResRW <- list()
+DT_hh <- as.xts(DT_hh)
+load(paste0(dFile, "houses.Rda"))
+for (house in houses){
   p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1) # compute forecast
-  sdResRW[[house]] <- sd(resid(p), na.rm = TRUE)
-  pMdl <- cbind(DT_hh$hHour[DT_hh$linkID == house], p$x, p$fitted) # create new df of time, actual, fitted
-  pMdl <- data.table(pMdl)
-  names(pMdl) <- c("Time", "Actual", "Fitted")
-  pMdl$Time <- lubridate::as_datetime(pMdl$Time, tz = 'Pacific/Auckland')
-  pMdl <- melt(pMdl, id = "Time")  # reshapes to format more conducive to ggplot
-  pMdl <- dplyr::arrange(pMdl, Time) # rearranges chronologically
   assign(paste0(house, "_model"), p) # assigns unique variable name
   get(paste0(house, "_model")) %>%
     save(file = paste0(dFile, "models/randomWalk/", house, "_at_30.Rda"))
-  assign(paste0(house, "_for_plotting"), pMdl)
-  get(paste0(house, "_model")) %>%
-    save(file = paste0(dFile, "models/randomWalk/plotting/", house, "_at_30_for_plotting.Rda"))
-  p <- ggplot(data = get(paste0(house, "_for_plotting"))[48:(48*3), ], aes(x = Time)) +
-    geom_line(aes(y = value, colour = variable)) +
-    labs(y = "Power (W)", colour = "")
-  ggsave(filename = paste0(pFile, "randomWalk/", house, "RW.pdf"))
+  plotModel(p, "randomWalk")
 }
-sdResRW <- as.data.frame(sdResRW)
-sdResRW <- as.data.frame(t(sdResRW)) # transposes for ease of computation
-save(sdResRW, file = paste0(dFile, "models/randomWalk/sdResRW.Rda"))
+#sdResRW <- as.data.frame(sdResRW)
+#sdResRW <- as.data.frame(t(sdResRW)) # transposes for ease of computation
+#save(sdResRW, file = paste0(dFile, "models/randomWalk/sdResRW.Rda"))
 
 # This times the model
 proc.time() <- RWproc
-for (house in unique(DT_hh$linkID)){
+for (house in houses){
   p <- forecast::naive(DT_hh$HWelec[DT_hh$linkID == house], h = 1)
 }
 RWproc <- proc.time()
