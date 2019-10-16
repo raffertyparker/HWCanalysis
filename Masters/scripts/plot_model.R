db6b01f47327db12e978443bac48de7f3f3e2834
Rafferty Parker <raffertyjparker@gmail.com>
2019-10-16 17:28:00 +1300

    updated to display midnight to midnight

--- Masters/scripts/plot_model.R
+++ Masters/scripts/plot_model.R
@@ -3,12 +3,16 @@ plotModel <- function(ModelData, ModelName){
   pMdl <- as.data.table(ModelData$x)
   names(pMdl) <- c("Time", "Actual")
   pMdl$Fitted <- ModelData$fitted
-  pMdl <- melt(for_plotting, id = "Time") 
+  pMdl <- melt(pMdl, id = "Time") 
   pMdl <- dplyr::arrange(pMdl, Time) # rearranges chronologically
-  ggplot(data = pMdl[48:(48*3), ], aes(x = Time)) +
+  pMdl$Time <- lubridate::as_datetime(pMdl$Time, # convert from UTC
+                                      tz = 'Pacific/Auckland')
+  startTime <- lubridate::as_datetime(paste(as.character( # startTime is midnight on first day of next month
+    rollback(pMdl$Time[1] + dweeks(5), roll_to_first = TRUE, preserve_hms = FALSE)), "00:00:00"), 
+    tz = 'Pacific/Auckland')
+  ggplot(data = pMdl[pMdl$Time %within% interval(startTime, startTime + days(1)), ], # Select data 1 day from startTime
+         aes(x = Time)) +
     geom_line(aes(y = value, colour = variable)) +
     labs(y = "Power (W)", colour = "")
   ggsave(filename = paste0(pFile, ModelName,"/", house, ".pdf"))
 }
-
-
