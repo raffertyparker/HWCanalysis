82dff2f34f6a6622d98a697eddd8043882afe409
Rafferty Parker <raffertyjparker@gmail.com>
2019-11-12 16:23:16 +1300

    Added variance plots (density and bar) and table. Included rudimentary interpretability/fidelity metric

--- Masters/scripts/model_compare_plot.R
+++ Masters/scripts/model_compare_plot.R
@@ -7,9 +7,46 @@ if (!exists("pFile")){
   pFile <- "~/HWCanalysis/Masters/plots/" 
 }
 
+# Plot of the variance of the models
+DF <- readRDS(file = paste0(dFile, "allHouseModelStats.rds"))
+DF$model <- factor(DF$model, levels = c("naive","seasonalNaive","simpleLinear",
+                                                          "ARIMA","ARIMAX","SARIMA"), 
+                            labels = c("Naive","Seasonal Naive","Linear Regression",
+                                       "ARIMA","ARIMAX","SARIMA"))
+
+p <- ggplot(DF, aes(x = RMSE, group = model)) +
+  geom_density() +
+  facet_grid(model ~.)
+ggsave(filename = paste0(pFile, "modelRMSEdensity.png"))
+ggsave(filename = paste0(pFile, "modelRMSEdensity.pdf"))
+
+p <- ggplot(DF, aes(x = RMSE, group = model)) +
+  geom_bar() +
+  facet_grid(model ~.)
+ggsave(filename = paste0(pFile, "modelRMSEbar.png"))
+ggsave(filename = paste0(pFile, "modelRMSEbar.pdf"))
+
+
+modelVariance <- DF %>%
+  group_by(model) %>%
+  summarise(variance = var(RMSE))
+saveRDS(modelVariance, file = paste0(dFile, "modelVariance.rds"))
+
+
 
 df <- readRDS(file = paste0(dFile, "AllModelSummaryStats.rds"))
 
+# This inputs the qualitative values for fidelity/interpretability
+# There is probably a better way of doing this
+df$interp_fidelity <- rep(0, nrow(df))
+df$interp_fidelity[df$model == "ARIMA"] <- 0.69
+df$interp_fidelity[df$model == "SARIMA"] <- 0.35
+df$interp_fidelity[df$model == "ARIMAX"] <- 0.25
+df$interp_fidelity[df$model == "naive"] <- 1 
+df$interp_fidelity[df$model == "seasonalNaive"] <- 1
+df$interp_fidelity[df$model == "simpleLinear"] <- 0.66
+  
+    
 df_adjusted <- df
 df_adjusted$RMSE <- 1-(df$RMSE - min(df$RMSE))/(max(df$RMSE) - min(df$RMSE))
 df_adjusted$fittingTime <- 1-(df$fittingTime - min(df$fittingTime))/(max(df$fittingTime) - min(df$fittingTime))
@@ -17,8 +54,8 @@ df_adjusted$memSize <- 1-(df$memSize - min(df$memSize))/(max(df$memSize) - min(d
 
 df_adjusted <- reshape2::melt(df_adjusted)
 
-df_adjusted$variable <- factor(df_adjusted$variable, levels = c("RMSE", "fittingTime", "memSize"),
-                               labels = c("Accuracy", "Speed", "Compactness"))
+df_adjusted$variable <- factor(df_adjusted$variable, levels = c("RMSE", "fittingTime", "memSize", "interp_fidelity"),
+                               labels = c("Accuracy", "Speed", "Compactness", "Fidelity/Interpretability"))
 df_adjusted$model <- factor(df_adjusted$model, levels = c("naive","seasonalNaive","simpleLinear",
                                                           "ARIMA","ARIMAX","SARIMA"), 
                             labels = c("Naive","Seasonal Naive","Linear Regression",
@@ -38,6 +75,6 @@ ggsave(filename = paste0(pFile, "modelSummary.png"))
 ggsave(filename = paste0(pFile, "modelSummary.pdf"))
 
 
-p <- ggRadar(df_adjusted, aes(group = model), 
-             alpha = 0, scales = "free",
-             size = 0)
+#p <- ggRadar(df_adjusted, aes(group = model), 
+#             alpha = 0, scales = "free",
+#             size = 0)
