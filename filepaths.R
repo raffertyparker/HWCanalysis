c9d4b3c0f943ab75c0ab73c08dfc4738f9278de8
Rafferty Parker <raffertyjparker@gmail.com>
2020-01-24 15:00:41 +1300

    Initial commit

---
+++ filepaths.R
@@ -0,0 +1,10 @@
+# Enter filepaths here
+
+# path to scripts folder
+sFile <- "~/HWCanalysis/Masters/scripts/"
+
+# path to data folder
+dFile <- "~/HWCanalysis/Masters/data/" 
+
+# path to plot folder
+pFile <- "/home/parra358/HWCanalysis/Masters/plots/"  # (laTeX did not like '~' notation)
