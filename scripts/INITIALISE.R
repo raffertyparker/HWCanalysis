6c6c232e2f58b39363c4d8a16c4ec32ff6bff288
Rafferty Parker <raffertyjparker@gmail.com>
2020-01-31 14:13:57 +1300

    Added explanations of scripts being called

--- scripts/INITIALISE.R
+++ scripts/INITIALISE.R
@@ -8,9 +8,9 @@ source(paste0(sFile, "installPackages.R"))
 source(paste0(sFile, "imputeTotalPower.R")) 
 source(paste0(sFile, "extractCircuitFromCleanGridSpy1min.R")) 
 #source(paste0(sFile, "element_rating.R")) # Discontinued, not useful
-source(paste0(sFile, "processing.R")) 
+source(paste0(sFile, "processing.R")) # Processes the data into the required format
 source(paste0(sFile, "acv_plot.R")) # This creates ACV plots
 source(paste0(sFile, "freq_analysis.R")) # Calculates the frequency analysis
-source(paste0(sFile, "linear_plot_one_house.R"))
-source(paste0(sFile, "average_demand.R"))
-source(paste0(sFile, "modelling.R"))
+source(paste0(sFile, "linear_plot_one_house.R")) # Creates linear regression plots
+source(paste0(sFile, "average_demand.R")) # Creates average demand plots
+source(paste0(sFile, "modelling.R")) # Creates the models
