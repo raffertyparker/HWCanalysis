78e625823ff044d73181a0af362c749486459b2e
Rafferty Parker <raffertyjparker@gmail.com>
2020-02-26 16:53:50 +1300

    file -> folder

--- scripts/ccf_plot.R
+++ scripts/ccf_plot.R
@@ -1,14 +1,14 @@
 # CCV PLOTS #
 ###############################################
 
-if (!exists("pFile")){
-  pFile <- "~/HWCanalysis/plots/"
+if (!exists("pFolder")){
+  pFolder <- "~/HWCanalysis/plots/"
 }
-if (!exists("dFile")){
-  dFile <- "~/HWCanalysis/data/" 
+if (!exists("dFolder")){
+  dFolder <- "~/HWCanalysis/data/" 
 }
 if (!exists("DT")){
-  load(paste0(dFile, "DT.Rda"))
+  load(paste0(dFolder, "DT.Rda"))
 }
 
 library(ggplot2)
@@ -32,7 +32,7 @@ for (house in unique(DT$linkID)){
 
 ccvDT <- data.table(ccvDT)
 
-save(ccvDT, file = paste0(dFile, "ccvDT.Rda"))
+save(ccvDT, file = paste0(dFolder, "ccvDT.Rda"))
 
 # The following finds the value of ccvDT that has the maximum correlation
 # for each household and for lags greater than 30 mins
@@ -42,27 +42,28 @@ maxCor <- ccvDT %>%
   filter(lag >= 30) %>%
   slice(which.max(value))
 
-save(maxCor, file = paset0(dFile, "ccv_max.Rda"))
+save(maxCor, file = paset0(dFolder, "ccv_max.Rda"))
 
 ggplot(maxCor, aes(x = lag,fill=household)) +
   geom_histogram() + 
   labs(x = "Lag (minutes)", y = "Count", fill = "Household")
-ggsave(paste0(pFile, "maxXcorAllHouses.pdf"))
+ggsave(paste0(pFolder, "maxXcorAllHouses.pdf"))
 
 # This could do with being edited to feed variables into a function
 # Rather than just copy/pasted
 
 ggplot(ccvDT[ccvDT$lag > 0, ], aes(lag,value,colour=household)) +
   geom_line() + 
-  labs(x = "Lag (minutes)", y = "Cross-correlogram", colour = "House")
-ggsave(paste0(pFile, "ccfAllHouses.pdf"))
+  labs(x = "Lag (minutes)", y = "Cross-covariance", colour = "House")
+ggsave(paste0(pFolder, "ccfAllHouses.pdf"))
+ggsave(paste0(pFolder, "ccfAllHouses.png"))
 
 ggplot(ccvDT[ccvDT$lag > 0 & ccvDT$lag < 60, ], aes(lag,value,colour=household)) +
   geom_line() + 
-  labs(x = "Lag (minutes)", y = "Cross-correlogram", colour = "House")
-ggsave(paste0(pFile, "ccfAllHouses1Hour.pdf"))
+  labs(x = "Lag (minutes)", y = "Cross-covariance", colour = "House")
+ggsave(paste0(pFolder, "ccfAllHouses1Hour.pdf"))
 
 ggplot(ccvDT[ccvDT$lag > 0 & ccvDT$lag < 120, ], aes(lag,value,colour=household)) +
   geom_line() + 
-  labs(x = "Lag (minutes)", y = "Cross-correlogram", colour = "House")
-ggsave(paste0(pFile, "ccfAllHouses2Hour.pdf"))
+  labs(x = "Lag (minutes)", y = "Cross-covariance", colour = "House")
+ggsave(paste0(pFolder, "ccfAllHouses2Hour.pdf"))
--- scripts/element_plot.R
+++ scripts/element_plot.R
@@ -1,23 +1,24 @@
 # This plots the raw hot water data for one or two days to demonstrate the operation of the element
 # and the difference between data at 1 min and 30 min resolutions
 
-if (!exists("dFile")){
-  dFile <- "~/HWCanalysis/data/" 
+if (!exists("dFolder")){
+  dFolder <- "~/HWCanalysis/data/" 
 }
-if (!exists("pFile")){
-  pFile <- "~/HWCanalysis/plots/" 
+if (!exists("pFolder")){
+  pFolder <- "~/HWCanalysis/plots/" 
 }
 if (!exists("DT")){
-  load(paste0(dFile, "DT.Rda"))
+  load(paste0(dFolder, "DT.Rda"))
 }
 if (!exists("DT_hh")){
-  load(paste0(dFile, "DT_hh.Rda"))
+  load(paste0(dFolder, "DT_hh.Rda"))
 }
 library(dplyr)
 library(data.table)
 library(ggplot2)
 library(gridExtra)
 
+theme_set(theme_minimal())
 oneDay <- DT[dateTime_nz %between% c("2015-08-01", "2015-08-02") & linkID == "rf_06"]
 
 # HW elec and other appliance elec plot
@@ -28,7 +29,7 @@ pData[variable == "nonHWelec", variable := "Other appliances"]
 p4 <- ggplot(data = pData, aes(x = dateTime_nz, y = value, colour = variable)) +
   geom_line()
 p4 + labs(y = "Power (W)", x = "Time", colour = "")
-ggsave(filename = paste0(pFile, "bothElecPlot.pdf"))
+ggsave(filename = paste0(pFolder, "bothElecPlot.pdf"))
 
 # superceded by the above version
 #p3 <- ggplot(data = oneDay, aes(x = dateTime_nz)) +
@@ -42,18 +43,16 @@ p1 <- ggplot(data = oneDay, aes(x = dateTime_nz)) +
   geom_line(aes(y = HWelec)) +
   labs(y = "Power (W)", x = "")
 p1# + labs(y = "Power (W)", x = "Time")
-ggsave(filename = paste0(pFile, "oneDay.pdf"))
-ggsave(filename = paste0(pFile, "oneDay.png"))
-
-
+ggsave(filename = paste0(pFolder, "oneDay.pdf"))
+ggsave(filename = paste0(pFolder, "oneDay.png"))
 
 
 twoDay <- DT[dateTime_nz %between% c("2015-08-01", "2015-08-03") & linkID == "rf_06"]
 p <- ggplot(data = twoDay, aes(x = dateTime_nz)) +
   geom_line(aes(y = HWelec)) 
 p + labs(y = "Power (W)", x = "Time")
-ggsave(filename = paste0(pFile, "twoDay.pdf"))
-ggsave(filename = paste0(pFile, "twoDay.png"), width = 16, height = 5.7)
+ggsave(filename = paste0(pFolder, "twoDay.pdf"))
+ggsave(filename = paste0(pFolder, "twoDay.png"), width = 16, height = 5.7)
 
 DT_hh <- as.data.table(DT_hh)
 oneDay30min <- DT_hh[hHour %between% c("2015-08-01", "2015-08-02") & linkID == "rf_06"]
@@ -63,13 +62,14 @@ p2 <- ggplot(data = oneDay30min, aes(x = hHour)) +
   labs(y = "Power (W)", x = "Time") # + , colour="#F8766D" 
 #  theme(legend.position="none") Necessary for colour
 p2 #+ labs(y = "Power (W)", x = "Time")
-ggsave(filename = paste0(pFile, "oneDay30min.pdf"))
-ggsave(filename = paste0(pFile, "oneDay30min.png"))
+ggsave(filename = paste0(pFolder, "oneDay30min.pdf"))
+ggsave(filename = paste0(pFolder, "oneDay30min.png"))
 
 # Create comparison plot
 
 compPlot <- grid.arrange(p1,p2,ncol=1)
-ggsave(filename = paste0(pFile, "elementComparisonPlot.pdf"))
+ggsave(compPlot, filename = paste0(pFolder, "elementComparisonPlot.pdf"))
+ggsave(compPlot, filename = paste0(pFolder, "elementComparisonPlot.png"))
 
 
 twoDay30min <- DT_hh[hHour %between% c("2015-08-01", "2015-08-03") & linkID == "rf_06"]
@@ -78,5 +78,5 @@ p <- ggplot(data = twoDay30min, aes(x = hHour)) +
   geom_line(aes(y = HWelec))
 #  theme(legend.position="none") Necessary for colour
 p + labs(y = "Power (W)", x = "Time")
-ggsave(filename = paste0(pFile, "twoDay30min.pdf"))
-ggsave(filename = paste0(pFile, "twoDay30min.png"), width = 16, height = 5.7)
+ggsave(filename = paste0(pFolder, "twoDay30min.pdf"))
+ggsave(filename = paste0(pFolder, "twoDay30min.png"), width = 16, height = 5.7)
