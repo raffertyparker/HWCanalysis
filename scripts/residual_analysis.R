1b9f2fb7223ed7dd152ad58e3c4d6537864e4d21
Rafferty Parker <raffertyjparker@gmail.com>
2020-02-27 16:14:56 +1300

    Included peak shading

--- scripts/residual_analysis.R
+++ scripts/residual_analysis.R
@@ -16,6 +16,38 @@ theme_set(theme_minimal())
 
 load(paste0(dFolder, "houses.Rda"))
 
+
+# ---- define peak periods ----
+# Add peaks function adapted from original written by Ben Anderson
+# for project https://github.com/CfSOtago/evAnalysis/
+
+amPeakStart <- 7
+amPeakEnd <- 9
+pmPeakStart <- 17
+pmPeakEnd <- 20
+# Functions ----
+peaksAlpha <- 0.08
+peaksCol <- "#0072B2" # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/#a-colorblind-friendly-palette
+addPeaks <- function(p){
+  # takes a plot, assumes x is hms, adds peak period annotations
+  # assumes you've set yMin & yMax already
+  # breaks with facet_grid, scales = "free" so you have to build seperate plots if you want to do that
+  # there is a complex solution (https://stackoverflow.com/questions/27898651/get-scales-range-of-facets-scales-free) but...
+  p <- p + annotate("rect", xmin = amPeakStart, # set earlier
+                    xmax = amPeakEnd, # set earlier
+                    ymin = yMin, ymax = yMax, 
+                    alpha = peaksAlpha, fill = peaksCol)
+  p <- p + annotate("rect", xmin = pmPeakStart, # set earlier
+                    xmax = pmPeakEnd, # set earlier
+                    ymin = yMin, ymax = yMax, 
+                    alpha = peaksAlpha, fill = peaksCol)
+  return(p)
+}
+
+# ------------------------------------------
+
+
+
 models <- c("STLARIMA", "naive","seasonalNaive","simpleLinear", "ARIMA", "ARIMAX", "SVM")
 #house <- "rf_06"
 #model <- "seasonalNaive"
@@ -46,17 +78,25 @@ for (model in models){
   }
 
   plotResDF <- melt(avResDF, id = "dHour")
+  
+  yMin <- min(plotResDF$value, na.rm = TRUE)
+  yMax <- max(plotResDF$value, na.rm = TRUE)
+
+  
   p <- ggplot(plotResDF, aes(x = dHour, y = value, colour = variable)) +
     geom_line() 
-  p + labs(x = "Hour", y = "Residual", 
+  p <- p + labs(x = "Hour", y = "Residual", 
          colour = "Household")
+  p <- addPeaks(p)
+  
   ggsave(paste0(pFolder, model, "/allHousesResidual.pdf"))
   ggsave(paste0(pFolder, model, "/allHousesResidual.png"))
   
   
   p <- ggplot(plotResDF, aes(x = dHour, y = value, group = variable)) +
     geom_boxplot() 
-  p + labs(x = "Hour", y = "Residual")
+  p <- p + labs(x = "Hour", y = "Residual")
+  p <- addPeaks(p)
   ggsave(paste0(pFolder, model, "/allHousesResidualBoxplot.pdf"))
   ggsave(paste0(pFolder, model, "/allHousesResidualBoxplot.png"))
 }
