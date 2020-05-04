82e920da1beba0df6ee54013145485a629ebd7e0
Rafferty Parker <raffertyjparker@gmail.com>
2020-05-05 10:42:11 +1200

    Specified plot sizes

--- scripts/average_demand.R
+++ scripts/average_demand.R
@@ -1,20 +1,20 @@
 # This script plots averages of hot water use over a day
 
-if (!exists("dFile")){
-  dFile <- "~/HWCanalysis/data/" 
+if (!exists("dFolder")){
+  dFolder <- "~/HWCanalysis/data/" 
 }
-if (!exists("pFile")){
-  pFile <- "~/HWCanalysis/plots/" 
+if (!exists("pFolder")){
+  pFolder <- "~/HWCanalysis/plots/" 
 }
 if (!exists("DT_hh")){
-  load(paste0(dFile, "DT_hh.Rda"))
+  load(paste0(dFolder, "DT_hh.Rda"))
 }
 
 library(dplyr)
 library(ggplot2)
 library(lubridate)
 library(data.table)
-theme_set(theme_minimal())
+theme_set(theme_minimal(base_size = 14))
 
 DT_hh$dayHour <- hour(DT_hh$hHour)
 
@@ -25,42 +25,44 @@ DT_hh$weekHour <- (lubridate::day(DT_hh$hHour)-1)*24 + DT_hh$dayHour
 # day plot (all households)
 p <- ggplot(DT_hh, aes(x=dayHour, y=HWelec))+
   stat_summary(fun.y="mean", geom="line") + 
-  facet_wrap(~linkID, ncol = 4, scales = "free_y")
+  facet_wrap(~linkID, ncol = 3, scales = "free_y")
 p + labs(x = "Hour of day", y = "Average demand (W)")
-ggsave(filename = paste0(pFile, "averages/averageDemand.pdf"))
-ggsave(filename = paste0(pFile, "averages/averageDemand.png"), dpi = 100)
-ggsave(filename = paste0(pFile, "averages/averageDemandHighRes.png"))
+ggsave(filename = paste0(pFolder, "averages/averageDemand.pdf"), width = 210, height = 260, units = "mm")
+ggsave(filename = paste0(pFolder, "averages/averageDemand.png"), dpi = 100)
+ggsave(filename = paste0(pFolder, "averages/averageDemandHighRes.png"), width = 210, height = 260, units = "mm")
 
 # day plot (all households, other appliances)
 p <- ggplot(DT_hh, aes(x=dayHour, y=nonHWelec))+
   stat_summary(fun.y="mean", geom="line") + 
-  facet_wrap(~linkID, ncol = 4, scales = "free_y")
+  facet_wrap(~linkID, ncol = 3, scales = "free_y")
 p + labs(x = "Hour of day", y = "Average demand (W)")
-ggsave(filename = paste0(pFile, "averages/averageNONHWDemand.pdf"))
-ggsave(filename = paste0(pFile, "averages/averageNONHWDemand.png"))
+ggsave(filename = paste0(pFolder, "averages/averageNONHWDemand.pdf"))
+ggsave(filename = paste0(pFolder, "averages/averageNONHWDemand.png"))
 
 # week plot (all households)
 p <- ggplot(DT_hh, aes(x=weekHour, y=HWelec))+
   stat_summary(fun.y="mean", geom="line") + 
-  facet_wrap(~linkID, ncol = 4, scales = "free_y")
+  facet_wrap(~linkID, ncol = 3, scales = "free_y")
 p + labs(x = "Hour of week", y = "Average demand (W)")
-ggsave(filename = paste0(pFile, "averages/averageDemandWeek.pdf"))
-ggsave(filename = paste0(pFile, "averages/averageDemandWeek.png"))
+ggsave(filename = paste0(pFolder, "averages/averageDemandWeek.pdf"))
+ggsave(filename = paste0(pFolder, "averages/averageDemandWeek.png"))
 
 # day plot (individual households)
 for (house in unique(DT_hh$linkID)){
   p <- ggplot(DT_hh[linkID == house], aes(x=dayHour, y=HWelec))+
     stat_summary(fun.y="mean", geom="line", size = 1)
   p + labs(x = "Hour of day", y = "Average demand (W)")
-  ggsave(filename = paste0(pFile, "averages/", house, "averageDemand.pdf"))
-  ggsave(filename = paste0(pFile, "averages/", house, "averageDemand.png"))
+  ggsave(filename = paste0(pFolder, "averages/", house, "averageDemand.pdf"))
+  ggsave(filename = paste0(pFolder, "averages/", house, "averageDemand.png"))
 }
 
 # 2 day plot to contrast with averages
-load(paste0(dFile, "houses.Rda"))
+# Broken, easily fixed but no longer in use.
+
+load(paste0(dFolder, "houses.Rda"))
 for (house in houses){
   assign("tmpDT", 
-         as.data.table(readr::read_csv(paste0(dFile, "households/", 
+         as.data.table(readr::read_csv(paste0(dFolder, "households/", 
                                               house,"_at_30_min.csv"))))
 
 startTime <- lubridate::as_datetime(paste(as.character( # startTime is midnight on first day of next month
@@ -78,16 +80,17 @@ p <- ggplot(data = twoDay, aes(x = Hour)) +
               geom_line(aes(y = HWelec), size = 1) +
               facet_grid(rows = vars(Date), scales = "free_x")
             p + labs(x = "Hour of day", y = "Power (W)")
-            ggsave(filename = paste0(pFile, "averages/two_day_", house, ".png"))
-            ggsave(filename = paste0(pFile, "averages/two_day_", house, ".pdf"))
+            ggsave(filename = paste0(pFolder, "averages/two_day_", house, ".png"))
+            ggsave(filename = paste0(pFolder, "averages/two_day_", house, ".pdf"))
 }
 
 # This creates the boxplot of average demand
 p <- ggplot(DT_hh, aes(x = linkID, y = HWelec, group = linkID)) +
   geom_boxplot() +
+  stat_summary(fun.y=mean, geom="point", shape = 124, size=4, color="red") +
   labs(x = "Household", y = "Demand (W)")
 p + coord_flip() 
-ggsave(filename = paste0(pFile, "averages/boxplot.png"), dpi = 100)
-ggsave(filename = paste0(pFile, "averages/boxplotHighRes.png"))
-ggsave(filename = paste0(pFile, "averages/boxplot.pdf"))
+ggsave(filename = paste0(pFolder, "averages/boxplot.png"), dpi = 100)
+ggsave(filename = paste0(pFolder, "averages/boxplotHighRes.png"))
+ggsave(filename = paste0(pFolder, "averages/boxplot.pdf"))
 
--- scripts/ccf_plot.R
+++ scripts/ccf_plot.R
@@ -1,5 +1,4 @@
 # CCV PLOTS #
-###############################################
 
 if (!exists("pFolder")){
   pFolder <- "~/HWCanalysis/plots/"
@@ -7,15 +6,18 @@ if (!exists("pFolder")){
 if (!exists("dFolder")){
   dFolder <- "~/HWCanalysis/data/" 
 }
+#####################################################
+# Load presaved resaved data files and then manually make plots
+# makes things a lot faster!
+#load(paste0(dFolder, "ccvDT.Rda"))
+#load(paste0(dFolder, "ccv_max.Rda"))
+#####################################################
 if (!exists("DT")){
   load(paste0(dFolder, "DT.Rda"))
 }
 
 library(ggplot2)
-theme_set(theme_minimal())
-
-types <- c("acv", "ccv")
-
+theme_set(theme_minimal(base_size = 14))
 
 for (house in unique(DT$linkID)){
   q <- forecast::Ccf(DT$HWelec[DT$linkID == house], DT$nonHWelec[DT$linkID == house], lag.max = 60*24,
@@ -42,23 +44,32 @@ maxCor <- ccvDT %>%
   filter(lag >= 30) %>%
   slice(which.max(value))
 
-save(maxCor, file = paset0(dFolder, "ccv_max.Rda"))
+save(maxCor, file = paste0(dFolder, "ccv_max.Rda"))
 
 ggplot(maxCor, aes(x = lag,fill=household)) +
   geom_histogram() + 
   labs(x = "Lag (minutes)", y = "Count", fill = "")
 ggsave(paste0(pFolder, "maxXcorAllHouses.pdf"))
 
+
+ccvDT$lag <- ccvDT$lag/60
 # This could do with being edited to feed variables into a function
 # Rather than just copy/pasted
+
+ggplot(ccvDT[ccvDT$lag > -1 & ccvDT$lag < 8, ], aes(lag,value)) +
+  geom_line() + 
+  facet_wrap(~household, ncol = 3, scales = "free_y") +
+  labs(x = "Lag (hours)", y = "Cross-covariance")
+ggsave(paste0(pFolder, "ccfAllHouses.pdf"), width = 210, height = 260, units = "mm")
+ggsave(paste0(pFolder, "ccfAllHouses.png"), width = 210, height = 260, units = "mm")
 
 ggplot(ccvDT[ccvDT$lag > 0, ], aes(lag,value,colour=household)) +
   geom_line() + 
   labs(x = "Lag (minutes)", y = "Cross-covariance", colour = "") +
   theme(legend.position="top") +
   guides(colour=guide_legend(nrow=3,byrow=TRUE))
-ggsave(paste0(pFolder, "ccfAllHouses.pdf"))
-ggsave(paste0(pFolder, "ccfAllHouses.png"))
+ggsave(paste0(pFolder, "ccfColourAllHouses.pdf"))
+ggsave(paste0(pFolder, "ccfColourAllHouses.png"))
 
 ggplot(ccvDT[ccvDT$lag > 0 & ccvDT$lag < 60, ], aes(lag,value,colour=household)) +
   geom_line() + 
